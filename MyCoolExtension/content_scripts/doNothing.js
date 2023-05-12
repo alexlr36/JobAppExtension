dae7059791376aa487aab13e544397ec014fc018
A_R <u1175124@utah.edu>
2023-05-12 02:55:23 -0600

    jquery(better)-discrptns are good now

--- MyCoolExtension/content_scripts/doNothing.js
+++ MyCoolExtension/content_scripts/doNothing.js
@@ -1,38 +1,6 @@
 (function() {
 
-    function getJobInfo(){
-        jobs = [];
-        // Dig into jobs section
-        var unordered_list = document.getElementsByClassName("scaffold-layout__list-container")[0];
-        console.log("unordered_list:", unordered_list);
-        var list_items = unordered_list.getElementsByClassName("jobs-search-results__list-item");
-
-        for(var i = 0; i < list_items.length; i++){
-            var job_values = {};
-            k = 0;
-            for(const j in classnames){
-                console.log("classnames[j]:", classnames[j]);
-                var job_item = list_items[i].getElementsByClassName(classnames[j])[0].textContent;
-                job_item.trim();
-                job_item = job_item.replace(/^\s+|\s+$/gm,'');
-                job_values[k] = job_item;
-                k++;
-                console.log("job_item:", job_item);
-            };
-            // var title = list_items[i].getElementsByClassName(classnames.job_title_class)[0].textContent;
-            // var company = list_items[i].getElementsByClassName(classnames.job_company_class)[0].textContent;
-            // var location = list_items[i].getElementsByClassName(classnames.job_location_class)[0].textContent;
-            var description = " "; // description empty for now. Will need to go in an parse data eventually.
-            var logo = list_items[i].getElementsByClassName(job_image_src_class)[0].src;
-            logo = logo.replace(/^\s+|\s+$/gm,'');
-            job_values[k] = description;
-            job_values[k+1] = logo;
-            // appened to jobs list
-            jobs[i] = job_values
-            console.log("i:", i, ", jobs:", jobs);
-        }
-        return jobs;
-    }
+    const timer = ms => new Promise(res => setTimeout(res, ms));
     // Current names for the classes connected to jobs in linkedin.
     // making these vars since linkedin will probably change them eventually.
     var job_image_src_class = "evi-image";
@@ -42,6 +10,55 @@
         job_location_class: "job-card-container__metadata-item"
         //job_image_src_class: "evi-image"
     };
+
+    async function getJobInfo(){
+        const jobs = [];
+        console.log("yo");
+        // Select the job listings container
+        const jobListContainer = document.querySelector('.jobs-search-results-list');
+        
+        // Select all the job listings
+        const jobListings = jobListContainer.querySelectorAll('li');
+        
+        // Loop through each job listing
+        jobListings.forEach(jobListing => {
+          // Extract the job title
+          const titleElement = jobListing.querySelector('.job-card-list__title');
+          const title = titleElement ? titleElement.textContent.trim() : '';
+          if(title == "") {
+            return;
+            }
+          // Extract the company name
+          const companyElement = jobListing.querySelector('.job-card-container__primary-description');
+          const company = companyElement ? companyElement.textContent.trim() : '';
+        
+          // Extract the job location
+          const locationElement = jobListing.querySelector('.job-card-container__metadata-item');
+          const location = locationElement ? locationElement.textContent.trim() : '';
+        
+          // Wait for the description to load
+          setTimeout(() => {
+            // Simulate a click event on the job listing to load the description
+            jobListing.click();
+            // Extract the job description
+            const descriptionElement = document.querySelector('.jobs-description-content__text');
+            const description = descriptionElement ? descriptionElement.textContent.trim() : '';
+        
+            // Create a job object and add it to the jobs array
+            const job = {
+              title,
+              company,
+              location,
+              description
+            };
+            jobs.push(job);
+        
+            // Log the jobs array to the console
+            console.log(job);
+          }, 5000); // Wait for 2 seconds for the description to load
+        });        
+        return jobs;
+    }
     try {
         // Save jobs in an arrayList(for now)
         var jobs = getJobInfo();
